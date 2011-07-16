04d2effed7841d0df4cfb07cf586792da709c490
Bo Schwarzstein <Bo.Schwarzstein@gmail.com>
2011-07-16 16:30:16 +0800

    A.L.M.

---
+++ Stokes/Core/UnitTests/UnitTestField.hpp
@@ -0,0 +1,18 @@
+#include <cxxtest/TestSuite.h>
+
+#include <Stokes/Core/Field.hpp>
+
+class UnitTestField : public CxxTest::TestSuite
+{
+public:
+	void setUp()
+	{
+	}
+	void tearDown()
+	{
+	}
+
+	void testOverall()
+	{
+	}
+};
